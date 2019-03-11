8e5bc6a48a78e63a6fa16e19cdef0df4d0d0be40
Antonio Doslic <antonio.doslic@plavatvornica.com>
2019-03-11 13:57:06 +0100

    slider animation second part

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 //import {Container, Row, Col} from 'react-bootstrap';
 import './App.scss';
-import slika1 from './Img/slider-image-1.jpg';
-import slika2 from './Img/slider-image-2.jpg';
-import slika3 from './Img/slider-image-3.jpg';
-import slika4 from './Img/slider-image-4.jpg';
-import slika5 from './Img/slider-image-5.jpg';
-import slika6 from './Img/slider-image-6.jpg';
-import slika7 from './Img/slider-image-7.jpg';
-import slika8 from './Img/slider-image-8.jpg';
-import slika9 from './Img/slider-image-9.jpg';
+// import slika1 from './Img/slider-image-1.jpg';
+// import slika2 from './Img/slider-image-2.jpg';
+// import slika3 from './Img/slider-image-3.jpg';
+// import slika4 from './Img/slider-image-4.jpg';
+// import slika5 from './Img/slider-image-5.jpg';
+// import slika6 from './Img/slider-image-6.jpg';
+// import slika7 from './Img/slider-image-7.jpg';
+// import slika8 from './Img/slider-image-8.jpg';
+// import slika9 from './Img/slider-image-9.jpg';
 //import Dstrelica from './Img/arrow-gray-right.png';
 //import Lstrelica from './Img/arrow-gray-left.png';
 
@@ -23,10 +23,11 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      articles: []
+      articles: [],
+      width: 0
     }
   }
-  componentWillMount() {
+  componentDidMount() {
     let url = "https://newsapi.org/v2/top-headlines?q=&country=us&category=&apiKey=f100b3cb4ed147d192f5237f42e0970c";
     //console.log(url);
     fetch(url)
@@ -53,61 +54,127 @@ class App extends Component {
   }
     
   rightClick = () =>{
-    //console.log("desno");
-    let z = ReactDOM.findDOMNode(this.refs.firstRow);
+    
+    let firstRow = ReactDOM.findDOMNode(this.refs.firstRow);
+    let secondRow = ReactDOM.findDOMNode(this.refs.secondRow);
+    let firstChildFirstRow = ReactDOM.findDOMNode(this.refs.firstRow).firstChild;
+    let firstChildSecondRow = ReactDOM.findDOMNode(this.refs.secondRow).firstChild;
+    let widthFirstRow = firstChildFirstRow.clientWidth;
+
+    
+    //console.log("rig "+widthFirstRow);
+    let widthSecondRow = firstChildSecondRow.clientWidth;
 
     let promise = new Promise(
       function(resolve, reject) {
-        
-      let xyz = true;
-      z.classList.add('animation');
-      
-      ReactDOM.findDOMNode(this.refs.firstRow).append(ReactDOM.findDOMNode(this.refs.firstRow).firstChild);
-      
-      ReactDOM.findDOMNode(this.refs.secondRow).append(ReactDOM.findDOMNode(this.refs.secondRow).firstChild);
+        firstRow.classList.add('animation');
+        secondRow.classList.add('animation');
 
-      if(xyz){
-        if (z.classList.contains('animation')) {
-          //resolve(z.classList.remove('animation'));
-        }
-      }
-      else{
-        reject('reject');
-      }
+        firstRow.style.transform = "translate("+widthFirstRow+"px,0)";
+        secondRow.style.transform = "translate("+widthSecondRow+"px,0)";
+
+        firstChildFirstRow.classList.add('fade-out');
+        firstChildSecondRow.classList.add('fade-out');
+        setTimeout(() => resolve("done!"), 1000);
     });
 
-    promise.then(function(){
-      console.log("test");
+    promise.then( (resolveValue) =>{
+      if(resolveValue=='done!'){
+        firstRow.classList.remove('animation');
+        secondRow.classList.remove('animation');
+
+        //firstRow.style.transform = "translate(0,0)";
+        firstRow.style.transform = "none";
+        //secondRowsecondRow.style.transform = "translate(0,0)";
+        secondRow.style.transform = "none";
+
+        firstChildFirstRow.classList.remove('fade-out');
+        firstChildSecondRow.classList.remove('fade-out');
+        
+      }
+      ReactDOM.findDOMNode(this.refs.firstRow).append(firstChildFirstRow);
+      ReactDOM.findDOMNode(this.refs.secondRow).append(firstChildSecondRow);
+      //console.log("test");
     })
     
   }
   leftClick = () =>{
-    //console.log("lijevo");
+    
     //let x= this.refs.firstRow.lastChild;
-    let x= ReactDOM.findDOMNode(this.refs.firstRow).lastChild;
     //this.refs.firstRow.prepend(x);
-    ReactDOM.findDOMNode(this.refs.firstRow).prepend(x);
-
     /*let y= this.refs.secondRow.lastChild;
     this.refs.secondRow.prepend(y);*/
 
-    let y= ReactDOM.findDOMNode(this.refs.secondRow).lastChild;
-    ReactDOM.findDOMNode(this.refs.secondRow).prepend(y);
+    // let x= ReactDOM.findDOMNode(this.refs.firstRow).lastChild;
+    // ReactDOM.findDOMNode(this.refs.firstRow).prepend(x);
+    // let y= ReactDOM.findDOMNode(this.refs.secondRow).lastChild;
+    // ReactDOM.findDOMNode(this.refs.secondRow).prepend(y);
+
+    let firstRow = ReactDOM.findDOMNode(this.refs.firstRow);
+    let secondRow = ReactDOM.findDOMNode(this.refs.secondRow);
+    let firstChildFirstRow = ReactDOM.findDOMNode(this.refs.firstRow).firstChild;
+    let firstChildSecondRow = ReactDOM.findDOMNode(this.refs.secondRow).firstChild;
+    let lastChildFirstRow = ReactDOM.findDOMNode(this.refs.firstRow).lastChild;
+    let lastChildSecondRow = ReactDOM.findDOMNode(this.refs.secondRow).lastChild;
+    let widthfirstChildFirstRow = firstChildFirstRow.clientWidth;
+    let widthfirstChildSecondRow = lastChildSecondRow.clientWidth;
+    //console.log("left "+widthfirstChildFirstRow);
+    
+    let widthLastChildFirstRow = lastChildFirstRow.clientWidth;
+    console.log('widthlastChild :', widthLastChildFirstRow);
+
+    firstChildFirstRow.style.transition = "margin 1s"
+    
+    //lastChildFirstRow.before(firstChildFirstRow);
+    //ReactDOM.findDOMNode(this.refs.firstRow).prepend(lastChildFirstRow);
+    
+    
+    let promise = new Promise(
+      function(resolve, reject) {
+        firstRow.classList.add('animation');
+        secondRow.classList.add('animation');
+
+        firstRow.style.transform = "translate(-"+widthfirstChildFirstRow+"px,0)";
+        
+        secondRow.style.transform = "translate(-"+widthfirstChildSecondRow+"px,0)";
+
+        //lastChildFirstRow.classList.add('fade-in');
+        //lastChildSecondRow.classList.add('fade-in');
+        setTimeout(() => resolve("done!"), 1000);
+    });
+
+    promise.then( (resolveValue) =>{
+      if(resolveValue=='done!'){
+        firstRow.classList.remove('animation');
+        secondRow.classList.remove('animation');
+
+        //firstRow.style.transform = "translate(0,0)";
+        firstRow.style.transform = "none";
+        secondRow.style.transform = "none";
+
+        //secondRow.style.transform = "translate(0,0)";
+        
+        //lastChildFirstRow.classList.remove('fade-in');
+        //lastChildSecondRow.classList.remove('fade-in');
+      }
+      ReactDOM.findDOMNode(this.refs.firstRow).prepend(lastChildFirstRow);
+      ReactDOM.findDOMNode(this.refs.secondRow).prepend(lastChildSecondRow);
+    })
   }
+
+
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <div className="left">
-            <div>
               
               <FirstRowImage
                 ref="firstRow"
-                //className={this.state.active ? 'animation': null} 
                 articles={this.state.articles.slice(0, Math.round(this.state.articles.length/2)+1)}
-                //active={this.state.active}
               />
-
+                
               <ul className="arrows">
                 <li className="Dstrelica"
                   onClick={this.rightClick}>
@@ -124,7 +191,6 @@ class App extends Component {
                  articles={this.state.articles.slice(Math.round(this.state.articles.length/2)+1, this.state.articles.length)}
                  //length={this.state.articleLength}
               />
-            </div>
           </div>
           <div className="right">
             <h2>Lorem ipsum dolor sit amet consectetur adipiscing elit</h2>
--- src/Components/firstRowImage/index.js
+++ src/Components/firstRowImage/index.js
@@ -5,6 +5,7 @@ import slika1 from '../../Img/slider-image-1.jpg';
 //import slika4 from '../../Img/slider-image-4.jpg';
 //import slika5 from '../../Img/slider-image-5.jpg';
 //const array = [slika1, slika2, slika3, slika4, slika5];
+
 class firstRowImage extends Component {
     constructor(props) {
         super(props);
@@ -13,14 +14,29 @@ class firstRowImage extends Component {
     
     render() {
         return (
-                
                 <ul className="firstRow">
                     {(this.props.articles.length>0)?
                         this.props.articles.map((article, index) => (
-                             <li><img src={this.props.articles[index].urlToImage}/></li>
+                            (index==0)?
+                            <li /*className="Opacity"*/
+                                key={index}>
+                                <img src={this.props.articles[index].urlToImage}/>
+                            </li>
+                            :
+                            <li key={index}>
+                                <img src={this.props.articles[index].urlToImage}/>
+                            </li>
                         ))
                         :
                         <li><img src={slika1}/></li>
+                    }
+                    {
+                        // (img.classList.contains("Opacity"))?{
+                        //     this.classList.add
+                        // }
+                        // :{
+                        //     //
+                        // }
                     }
                 </ul>
         );
